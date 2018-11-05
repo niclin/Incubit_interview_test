71d1c298e07583b6e498945b05b6cfe8e3ce63f3
niclin <bboyceo@hotmail.com>
2018-11-05 18:15:19 +0800

    Add user welcome mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default :from => "me@mydomain.com"
+
+  def welcome(user_id)
+    @user = User.find(user_id)
+
+    mail(to: "#{user.name} #{user.email}", subject: "Welcome")
+  end
+end
