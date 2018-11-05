79527f82d08daa68514b1eb8acd981ba5ec8156c
niclin <bboyceo@hotmail.com>
2018-11-06 00:01:33 +0800

    Add reset password Valid/Wrong email test

--- spec/features/password_reset_spec.rb
+++ spec/features/password_reset_spec.rb
@@ -1,5 +1,5 @@
 RSpec.feature "User password resets" do
-  scenario "password resets" do
+  scenario "Valid email password resets" do
     create(:user, email: "user@example.com", password: "password", password_confirmation: "password")
 
     visit "/login"
@@ -14,5 +14,21 @@ RSpec.feature "User password resets" do
 
 
     expect(ActionMailer::Base.deliveries.size).to eq(1)
+  end
+
+  scenario "Wrong email password resets" do
+    create(:user, email: "user@example.com", password: "password", password_confirmation: "password")
+
+    visit "/login"
+
+    click_link "forgot password"
+
+    within(".password_reset") do
+      fill_in "Email", with: "user@example.com"
+    end
+
+    click_button "Submit"
+
+    expect(page).to have_content("Welcome")
   end
 end
