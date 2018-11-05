5dab5d340fc98e01a521f4b3d9fe3a27627f3027
niclin <bboyceo@hotmail.com>
2018-11-05 18:07:08 +0800

    Redirect to profile page if user signup successfuly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # show successful message
+      flash[:success] = "Singup successful!"
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
